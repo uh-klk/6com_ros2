33081abd2fe3333954d05ced885b20eca5c0ab05
khenglee <k.l.koay@herts.ac.uk>
2025-04-04 17:24:59 +0100

    added my first commit

---
+++ src/my_py_pkg/setup.py
@@ -0,0 +1,27 @@
+from setuptools import find_packages, setup
+
+package_name = 'my_py_pkg'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='ros2user',
+    maintainer_email='ros2user@todo.todo',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'publisher=my_py_pkg.publisher:main',
+            'subscriber=my_py_pkg.subscriber:main',
+        ],
+    },
+)
